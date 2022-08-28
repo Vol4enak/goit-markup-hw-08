e5c305284a41bf36ec5ec10a0538384ec0920c86
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2022-08-28 18:59:27 +0300

    123

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,10 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+  // const mobileMenu = document.querySelector('.js-menu-container');
+  // const openMenuBtn = document.querySelector('.js-open-menu');
+  // const closeMenuBtn = document.querySelector('.js-close-menu');
+  
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
